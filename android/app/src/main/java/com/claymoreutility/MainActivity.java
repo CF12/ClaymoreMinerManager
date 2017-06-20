dc168842475b76aa6d6267337c66a93f66f95de9
CF12 <brianx0011@gmail.com>
2017-06-19 21:12:04 -0400

    Initial Commit

---
+++ android/app/src/main/java/com/claymoreutility/MainActivity.java
@@ -0,0 +1,23 @@
+package com.claymoreutility;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "ClaymoreUtility";
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new ReactNativeDialogsPackage() // add this manager
+        );
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  DrawerLayoutAndroid,
+  Button,
+  ToolbarAndroid,
+  TouchableOpacity,
+  TextInput
+} from 'react-native'
+import { MaterialDialog } from 'react-native-material-dialog'
+import ActionButton from 'react-native-action-button'
+import DialogAndroid from 'react-native-dialogs'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+function getMinerStats() {
+  fetch(`http://192.168.1.6:3333`, {
+    method: 'GET'
+  })
+  .then((res) => { return res.json() }).then((res) => {
+    console.log(JSON.stringify(res))
+    alert(res)
+  })
+  .catch((err) => {
+    alert(err)
+    console.error(err)
+  })
+}
+
+export default class App extends Component {
+  constructor () {
+    super()
+
+    this.navigationView = (
+      <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <View style={{backgroundColor: 'black', width: '100%', height: '25%'}} />
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={{width: '100%', height: '10%'}}>
+            <Text style={{textAlign: 'center'}}>TESTING</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+
+    this.addMinerDialog = new DialogAndroid()
+    this.addMinerDialog.set({
+      title: 'Add Miner',
+      content: 'Blah Blah Blah',
+      positiveText: 'ADD'
+    })
+  }
+
+  render () {
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.left}
+        renderNavigationView={() => this.navigationView}>
+
+        <View style={{flex: 1}}>
+          <ToolbarAndroid
+            style={{height: 64, backgroundColor: '#1976D2'}}
+            title='Claymore Miner Manager'
+            titleColor='white'
+            actions={[{title: 'Settings', show: 'always'}]}
+          />
+          <ActionButton
+            buttonColor='#FFAB00'
+            onPress={() => { this.addMinerDialog.show() }}
+          />
+        </View>
+      </DrawerLayoutAndroid>
+    )
+  }
+}
+
+AppRegistry.registerComponent('ClaymoreUtility', () => App)
