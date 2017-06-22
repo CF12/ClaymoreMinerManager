3b30bd18fc6d1432b419b178c2d6a353c4f00fb6
CF12 <brianx0011@gmail.com>
2017-06-22 09:19:19 -0400

    Major design improvements: - Added functional FAB button and Modals - Sidebar / Drawer
    Working on adding miner cards

--- index.android.js
+++ index.android.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View,
   DrawerLayoutAndroid,
-  Button,
-  ToolbarAndroid,
   TouchableOpacity,
-  TextInput
+  TouchableNativeFeedback,
+  TextInput,
+  Modal
 } from 'react-native'
-import { MaterialDialog } from 'react-native-material-dialog'
 import ActionButton from 'react-native-action-button'
-import DialogAndroid from 'react-native-dialogs'
-import Icon from 'react-native-vector-icons/FontAwesome'
+// import Modal from 'react-native-modal'
+import { MKButton, MKColor } from 'react-native-material-kit'
+
+import { Button, Text, StyleProvider, Container, Header, Content, ListItem, CheckBox, Left, Right, Body, Title, Icon, } from 'native-base/src'
+import getTheme from './native-base-theme/components'
 
 function getMinerStats() {
   fetch(`http://192.168.1.6:3333`, {
@@ -29,9 +29,29 @@ function getMinerStats() {
   })
 }
 
+class FlatButton extends Component {
+  constructor (props) {
+    super(props)
+
+    console.log(this.props)
+  }
+
+  render () {
+    return (
+      <View style={{flex: 1, backgroundColor: this.props.buttonColor, width: 200, height: 64}} onPress={this.props.onPress}>
+        <Text> {this.props.text}asdhsdakjh </Text>
+      </View>
+    )
+  }
+}
+
 export default class App extends Component {
   constructor () {
     super()
+
+    this.state = {
+      showModal: false
+    }
 
     this.navigationView = (
       <View style={{flex: 1, backgroundColor: '#fff'}}>
@@ -43,34 +63,81 @@ export default class App extends Component {
         </TouchableOpacity>
       </View>
     )
-
-    this.addMinerDialog = new DialogAndroid()
-    this.addMinerDialog.set({
-      title: 'Add Miner',
-      content: 'Blah Blah Blah',
-      positiveText: 'ADD'
-    })
   }
 
   render () {
-    return (
-      <DrawerLayoutAndroid
-        drawerWidth={300}
-        drawerPosition={DrawerLayoutAndroid.positions.left}
-        renderNavigationView={() => this.navigationView}>
+    let openDrawer = () => {
+      this.drawer.openDrawer()
+    }
 
-        <View style={{flex: 1}}>
-          <ToolbarAndroid
-            style={{height: 64, backgroundColor: '#1976D2'}}
-            title='Claymore Miner Manager'
-            titleColor='white'
-          />
-          <ActionButton
-            buttonColor='#FFAB00'
-            onPress={() => { this.addMinerDialog.show() }}
-          />
-        </View>
-      </DrawerLayoutAndroid>
+    let showModal = () => {
+      this.setState({showModal: true})
+    }
+
+    let hideModal = () => {
+      this.setState({showModal: false})
+    }
+
+    return (
+      <StyleProvider style={getTheme()} >
+        <Container>
+          <DrawerLayoutAndroid
+            ref={_drawer => { this.drawer = _drawer }}
+            drawerWidth={300}
+            drawerPosition={DrawerLayoutAndroid.positions.left}
+            renderNavigationView={() => this.navigationView}>
+
+            <Header>
+              <Left>
+                <Button transparent onPress={() => { openDrawer() }}>
+                  <Icon name='menu' />
+                </Button>
+              </Left>
+              <Body>
+                <Title> Claymore Utility </Title>
+              </Body>
+              <Right />
+            </Header>
+
+            <ActionButton
+              buttonColor='#FFAB00'
+              onPress={() => {
+                showModal()
+              }}
+            />
+
+            <Modal animationType={'slide'} visible={this.state.showModal} onRequestClose={() => { hideModal() }}>
+              <View style={{backgroundColor: 'white', width: '100%', height: '100%'}}>
+                <Text style={{paddingLeft: 24, paddingRight: 24, paddingTop: 24, paddingBottom: 20, fontSize: 24, fontWeight: '500'}}>Add Miner</Text>
+
+                <View style={{paddingLeft: 24, paddingRight: 24, paddingBottom: 40}}>
+                  <Text style={{color: '#9E9E9E'}}> Miner Name </Text>
+                  <TextInput autoCorrect={false} />
+                  <Text style={{color: '#9E9E9E'}}> IP Address </Text>
+                  <TextInput autoCorrect={false} />
+                  <Text style={{color: '#9E9E9E'}}> Port </Text>
+                  <TextInput autoCorrect={false} />
+                </View>
+
+                <View style={{flex: 1, flexDirection: 'row', position: 'absolute', bottom: 0}}>
+                  <TouchableNativeFeedback onPress={() => { hideModal() }}>
+                    <View style={{flex: 0.5, justifyContent: 'center', alignItems: 'center', backgroundColor: '#f44a41', height: 64}}>
+                      <Text style={{color: 'white', fontWeight: '500', textShadowColor: 'rgba(0, 0, 0, 80)', textShadowRadius: 5, textShadowOffset: {width: 0.2, height: 0.2}}}> CANCEL </Text>
+                    </View>
+                  </TouchableNativeFeedback>
+                  <TouchableNativeFeedback>
+                    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#5de459', height: 64}}>
+                      <Text style={{color: 'white', fontWeight: '500', textShadowColor: 'rgba(0, 0, 0, 80)', textShadowRadius: 5, textShadowOffset: {width: 0.2, height: 0.2}}}> ADD </Text>
+                    </View>
+                  </TouchableNativeFeedback>
+                </View>
+
+              </View>
+            </Modal>
+
+          </DrawerLayoutAndroid>
+        </Container>
+      </StyleProvider>
     )
   }
 }
